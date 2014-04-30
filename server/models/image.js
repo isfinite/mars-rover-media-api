3fc40e20b0bc5413072db6c8d651a003e843406d
Lauren Smith <isfinite@gmail.com>
2014-04-30 01:21:05 -0500

    adding image model

---
+++ server/models/image.js
@@ -0,0 +1,37 @@
+exports.image = function() {
+	this.rover = '';
+	this.url = {
+		site: ''
+		, raw: ''
+		, web: ''
+		, label: ''
+	}
+	this.camera = {
+		cameraModelComponentList: ''
+		, cameraPosition: ''
+		, cameraModelType: ''
+		, cameraVector: ''
+		, subframeRect: ''
+		, mastEl: ''
+		, mastAz: ''
+		, instrument: ''
+		, xyz: ''
+	}
+	this.properties = {
+		type: ''
+		, width: null
+		, height: null
+		, filesize: null
+	}
+	this.timestamps = {
+		created: null
+		, captured: null
+		, added: null
+	}
+	this.location = {
+		site: null
+		, drive: null
+	}
+	this.sclk = '';
+	this.attitude = '';
+}
