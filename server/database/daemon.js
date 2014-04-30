a48c93689dc90d97d1290fa1ea7292528de3ee4c
Lauren Smith <isfinite@gmail.com>
2014-04-30 01:21:49 -0500

    adding getImageData, getWeather, models

--- server/database/daemon.js
+++ server/database/daemon.js
@@ -1,5 +1,7 @@
 var http = require('http')
-	, req = require('request');
+	, req = require('request')
+	, imgModel = require('../models/image').image
+	, solModel = require('../models/sol').sol;
 
 function getManifest(callback) {
 	req('http://mars.jpl.nasa.gov/msl-raw-images/image/image_manifest.json', function(err, resp, body) {
@@ -17,22 +19,70 @@ function getWeatherData(sol, callback) {
 	req('http://marsweather.ingenology.com/v1/archive/?sol=' + sol, callback);
 }
 
+function getImageData(image, callback) {
+	var newImage =  new imgModel();
+	require('http').get(image.urlList, function(resp) {
+		require('imagesize')(resp, function(err, res) {
+			newImage.rover = 'msl';
+			newImage.sclk = image.sclk;
+			newImage.attitude = image.attitude;
+
+			newImage.url.raw = image.urlList;
+			newImage.url.site = 'http://mars.jpl.nasa.gov/msl/multimedia/raw/?rawid=' + image.itemName;
+			newImage.url.label = image.pdsLabelUrl;
+
+			newImage.location.site = image.site;
+			newImage.location.drive = image.drive;
+			
+			newImage.timestamps.created = new Date().toISOString();
+			newImage.timestamps.captured = image.utc;
+			newImage.timestamps.added = image.dateAdded;
+			
+			newImage.properties.type = image.sampleType;
+			newImage.properties.width = res.width;
+			newImage.properties.height = res.height;
+			newImage.properties.filesize = resp.headers['content-length'];
+
+			newImage.camera.instrument = image.instrument;
+			newImage.camera.cameraModelComponentList = image.cameraModelComponentList;
+			newImage.camera.cameraPosition = image.cameraPosition;
+			newImage.camera.cameraModelType = image.cameraModelType;
+			newImage.camera.cameraVector = image.cameraVector;
+			newImage.camera.subframeRect = image.subframeRect;
+			newImage.camera.mastEl = image.mastEl;
+			newImage.camera.mastAz = image.mastAz;
+			newImage.camera.xyz = image.xyz;
+
+			callback(newImage);
+		});
+	});
+}
+
 exports.run = function() {
 	getManifest(function(data) {
-		var sols = data.sols.slice(0, 1);
+		var sols = data.sols.slice(1, 2)
+			, solsData = [];
 
 		do {
 			var item = sols.shift();
-			getWeatherData(item.sol, function(err, resp, body) {
-				var weather = JSON.parse(body);
-				getSolManifest(item, function(data) {
-					var images = data.images.slice(0);
-					do {
-						var image = images.shift();
-						console.log(image);
-					} while (images.length > 0);
+			if (item.num_images > 0) {
+				getWeatherData(item.sol, function(err, resp, body) {
+					var weather = JSON.parse(body)
+						, solData = new solModel();
+
+					solData.sol = item.sol;
+					solData.weather = weather;
+
+					getSolManifest(item, function(data) {
+						var images = data.images.slice(0);
+						do {
+							getImageData(images.shift(), function(img) {
+								solData.images.push(img);
+							});
+						} while (images.length > 0);
+					});
 				});
-			});
+			}
 		} while (sols.length > 0);
 
 	});
