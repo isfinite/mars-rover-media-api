7484ad6825b31b459deec340d914c27e89e99a1c
Lauren Smith <isfinite@gmail.com>
2014-05-02 16:11:37 -0500

    adding pad fn

--- server/config/util.js
+++ server/config/util.js
@@ -3,6 +3,15 @@
 var fs = require('fs')
 	, path = require('path');
 
+exports.pad = function(originalNum, width, padChar) {
+	var numAsString = originalNum.toString();
+	
+	// Return originalNum since its already at width
+	if (numAsString.length >= width) return originalNum;
+
+	return new Array(++width - numAsString.length).join(padChar || 0) + numAsString;
+}
+
 exports.walk = function(modulesPath, excludeDir, callback) {
 	fs.readdirSync(modulesPath).forEach(function(file) {
 		var newPath = path.join(modulesPath, file)
