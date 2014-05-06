4a120440b84282f7dfe7805fd91da955d68ca1ac
Lauren Smith <isfinite@gmail.com>
2014-05-05 19:23:28 -0500

    stats model

--- server/models/stats.js
+++ server/models/stats.js
@@ -1,10 +1,35 @@
 var driver = require('../config/driver');
 
-module.exports.stats = function(callback) {
-	driver.db.aggregate([
-		{ $unwind: '$images' }
-		, { $group: { _id: 'total_images', count: { $sum: 1 } }}
-	], function(err, doc) {
-		callback(doc.shift());
-	});
-}
+var Stats = (function() {
+
+	var _statsData = function(callback) {
+		driver.db.aggregate([
+			{
+				$unwind: '$images'
+			}
+			, {
+				$group: {
+					_id: 'total_images'
+					, count: {
+						$sum: 1
+					}
+				}
+			}
+		], function(err, doc) {
+			if (err) callback(err);
+			if (doc) {
+				callback(doc.shift());
+			} else {
+				callback({
+					err: 'Unable to find stats document'
+				});
+			}
+		});
+	}
+
+	return {
+		get: _statsData
+	}
+})();
+
+module.exports = Stats;
