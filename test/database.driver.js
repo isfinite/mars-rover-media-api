449db7f84c6cb84ef3ddc363654e58312eef0dad
Lauren Smith <isfinite@gmail.com>
2014-05-06 09:24:34 -0500

    test updates

--- test/database.driver.js
+++ test/database.driver.js
@@ -3,29 +3,45 @@ var driver = require('../server/config/driver')
 
 ///--- Tests
 
-module.exports.loadMongoDB = function(test) {
-	if (process.env.DB_URL) {
-		test.ok(process.env.DB_URL, 'MongoDB loaded');
+module.exports.loadMongoDB = {
+	setUp: function(callback) {
+		callback();
+	}
+	, loadMongoDB: function(test) {
+		if (process.env.DB_URL) {
+			test.ok(process.env.DB_URL, 'MongoDB loaded');
 
-		driver.loadDatabase(function() {
-			test.ok(driver.db, 'DB added to module exports');
+			driver.loadDatabase(function() {
+				test.ok(driver.collection, 'DB added to module exports');
+				test.done();
+			});
+		} else {
 			test.done();
-		});
-	} else {
-		test.done();
+		}
+	}
+	, tearDown: function(callback) {
+		driver.db.close();
+		driver.db = null;
+		callback();
 	}
 }
 
 module.exports.loadNeDB = {
 	setUp: function(callback) {
+		this.url = process.env.DB_URL;
 		delete process.env.DB_URL;
 		callback();
 	}
 	, loadDb: function(test) {
 		test.ok(!process.env.DB_URL, 'DB does not exist in .env');
 		driver.loadDatabase(function() {
-			test.ok(driver.db, 'DB added to module exports');
+			test.ok(driver.collection, 'DB added to module exports');
 			test.done();
 		});
+	}
+	, tearDown: function(callback) {
+		process.env.DB_URL = this.url;
+		driver.db = null;
+		callback();
 	}
 }
